56911ea0fb4a69f963d7c78e457e14de30ca943c
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-02-01 21:23:29 +0530

    shellroboshop

--- roboshop.sh
+++ roboshop.sh
@@ -2,6 +2,8 @@
 
 SG_ID="sg-028a0f9feae2f961e"
 AMI_ID="ami-0220d79f3f480ecf5"
+ZONE_ID="Z0397543RCC8GJCVQ50L"
+DOMAIN_NAME="siddharthais.online"
 
 for instance in $@
 do
@@ -20,6 +22,7 @@ do
          --query 'Reservations[].Instances[].PublicIpAddress' \
          --output text
        )
+       RECORD_NAME="$$DOMAIN_NAME" #siddharthais.online
   else 
         IP=$(
             aws ec2 describe-instances \
@@ -27,9 +30,31 @@ do
             --query 'Reservations[].Instances[].PrivateIpAddress' \
             --output text
         )
+        RECORD_NAME="$instance.$DOMAIN_NAME" #mongodb.siddharthais.online
 
 
 fi
 
     echo "IP Address : $IP"
+
+    aws route53 change-resource-record-sets \
+  --hosted-zone-id $ZONE_ID \
+  --change-batch '
+  {
+    "Comment": "Updating a record"
+    ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [
+            {
+                "Value"         : "'$IP'"
+            }
+            ]
+        }
+    }]
+  }
+  '
 done
