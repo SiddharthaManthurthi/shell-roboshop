05726575da1af3d745cd0b0332eb17d55983a871
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-02-01 22:13:16 +0530

    shellroboshop

---
+++ catalogue.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/mongodb"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+  echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+  exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE () {
+   if [ $1 -ne 0 ]; then
+      echo -e "$R $2 installation failed $N" | tee -a $LOGS_FILE
+      exit 1
+    else
+        echo -e "$G $2 installation successful $N" | tee -a $LOGS_FILE
+    fi
+}
+
+dnf module disable nodejs -y &>> $LOGS_FILE
+VALIDATE $? "Disabling NodeJS Module"
+
+dnf module enable nodejs:20 -y &>> $LOGS_FILE
+VALIDATE $? "Enabling NodeJS 20 Module"
+
+dnf install nodejs -y &>> $LOGS_FILE
+VALIDATE $? "Installing NodeJS 20"
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+VALIDATE $? "Adding sytem User"
+
+mkdir /app 
+VALIDATE $? "Creating  Directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>> $LOGS_FILE
+VALIDATE $? "Downloading Catalogue App"
