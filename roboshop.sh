d491c2a208a91ef97c8c0fd79fc2041550338564
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-02-01 20:06:30 +0530

    shellroboshop

---
+++ roboshop.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+SG_ID= "sg-028a0f9feae2f961e"
+AMI_ID= "ami-0220d79f3f480ecf5"
+
+for instance in $@
+do
+  aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --tag-specifications 
+  "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Reservations[0].Instances[0].
+  PrivateIpAddress' --output text
+done
