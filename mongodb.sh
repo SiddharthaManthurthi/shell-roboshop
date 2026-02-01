0629ed58ab6531b49ede9446883892959c7c2b46
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-02-01 21:47:08 +0530

    shellroboshop

---
+++ mongodb.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/mongodb"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+  echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+  exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE () {
+   if [ $1 -ne 0 ]; then
+      echo -e "$R $2 installation failed $N" | tee -a $LOGS_FILE
+      exit 1
+    else
+        echo -e "$G $2 installation successful $N"
+    fi
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Copying Mongo Repo"
+
+DNF INSTALL mongodb-org -y
+VALIDATE $? "Installing MongoDB Server"
+
+systemctl enable mongod
+VALIDATE $? "Enabling MongoDB Service"
+
+sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mongod.conf
+VALIDATE $? Allowing romote connections"
+
+systemctl restart mongod
+VALIDATE $? "Restarting MongoDB Service"
