f1a2e73a62e17d5fdce12f6899aa078946994f0a
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-02-02 15:39:06 +0530

    Shell Roboshop

--- shipping.sh
+++ shipping.sh
@@ -45,6 +45,7 @@ VALIDATE $? "Creating  Directory"
 
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip 
 VALIDATE $? "Downloading shipping App"
+
 cd /app
 VALIDATE $? "Moving to app directory"
 
@@ -67,9 +68,16 @@ VALIDATE $? "Copying shipping Service File"
 dnf install mysql -y &>> $LOGS_FILE
 VALIDATE $? "Installing MySQL"
 
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>> $LOGS_FILE
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>> $LOGS_FILE
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>> $LOGS_FILE
+mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use citites' &>> $LOGS_FILE
+if [$? -ne 0]; then
+
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>> $LOGS_FILE
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>> $LOGS_FILE
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>> $LOGS_FILE
+    VALIDATE $? "Loading shipping schema and data"
+else
+    echo -e "$Y shipping schema is already present, skipping $N" | tee -a $LOGS_FILE
+fi
 
 systemctl enable shipping 
 systemctl start shipping
