42c04cc6f2a37d4d16c4571430efb488bf31a57f
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-02-02 21:20:58 +0530

    Shell Roboshop

--- shipping.sh
+++ shipping.sh
@@ -66,6 +66,7 @@ dnf install mysql -y &>> $LOGS_FILE
 VALIDATE $? "Installing MySQL"
 
 mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities' &>> $LOGS_FILE
+
 if [ $? -ne 0 ]; then
 
     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>> $LOGS_FILE
