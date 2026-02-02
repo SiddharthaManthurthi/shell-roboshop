11998c645b6d381d0cea10a987386404fcc86d86
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-02-02 16:51:09 +0530

    Shell Roboshop

--- payment.sh
+++ payment.sh
@@ -65,4 +65,5 @@ systemctl daemon-reload &>> $LOGS_FILE
 
 systemctl enable payment  &>> $LOGS_FILE
 systemctl start payment  &>> $LOGS_FILE
+systemctl start payment.service
 VALIDATE $? "Starting and enabling payment"
