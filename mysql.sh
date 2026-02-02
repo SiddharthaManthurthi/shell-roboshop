0c3391e54dc87abefd40a5e07c0863d0f12d66e2
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-02-02 14:55:28 +0530

    Shell Roboshop

---
+++ mysql.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/mongodb"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+  echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+  exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE () {
+   if [ $1 -ne 0 ]; then
+      echo -e "$R $2 installation failed $N" | tee -a $LOGS_FILE
+      exit 1
+    else
+        echo -e "$G $2 installation successful $N" | tee -a $LOGS_FILE
+    fi
+}
+
+dnf install mysql-server -y &>> $LOGS_FILE
+VALIDATE $? "Installing MySQL Server"
+
+systemctl enable mysqld
+systemctl start mysqld  
+VALIDATE $? " Enabling and starting MySQL Service"
+
+#get the password from the user
+
+mysql_secure_installation --set-root-pass RoboShop@123 &>> $LOGS_FILE
+VALIDATE $? "Setting MySQL root password"
